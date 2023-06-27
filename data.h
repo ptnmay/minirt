093f5688ec83ed57a0e4175d4823349890290c54
ptnmay <pittinansaeyang@gmail.com>
2023-06-28 04:35:09 +0700

    ac

---
+++ data.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   data.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 04:06:57 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/06/28 04:34:07 by psaeyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATA_H
+# define DATA_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+
+typedef struct s_data
+{
+	t_ambi	ambi;
+	
+} t_data;
+
+typedef struct s_ambient
+{
+	char	a;
+	float	lightning;
+	float	r;
+	float	g;
+	float	b;
+	
+} t_ambi;
+
+typedef struct s_camera
+{
+	float	x;
+	float	y;
+	float	z;
+	float	vector;
+	float	fov;
+} t_cam;
+
+
+
+#endif
