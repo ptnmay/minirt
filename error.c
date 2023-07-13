dc363b7873795434de9d9691c9ad041cf41011da
Pittinan <105956506+ptnmay@users.noreply.github.com>
2023-07-13 10:27:45 +0700

    color amb. only int

--- error.c
+++ error.c
@@ -6,34 +6,34 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 23:08:48 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/07/13 03:29:43 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/07/13 10:22:34 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "data.h"
 
-void	error(char *s,char **str, int mode)
+void	error(char *s, int mode)
 {
-	int	i;
+	// int	i;
 	// int	j;
 
-	i = 0;
+	// i = 0;
 	// j = 0;
 	ft_putendl_fd(s, 2);
-	if (mode == 0 && str == NULL)
+	if (mode == 0)
 		exit(0);
-	else if (mode == 1)
-	{
-		while(str[i])
-		{
-			// while(str[i][j])
-			// {
-			// 	free(str[i][j]);
-			// 	j++;
-			// }
-			free(str[i]);
-			i++;
-		}
-		exit(0);
-	}
+	// else if (mode == 1)
+	// {
+	// 	while(str[i])
+	// 	{
+	// 		// while(str[i][j])
+	// 		// {
+	// 		// 	free(str[i][j]);
+	// 		// 	j++;
+	// 		// }
+	// 		free(str[i]);
+	// 		i++;
+	// 	}
+	// 	exit(0);
+	// }
 }
--- verify_0.c
+++ verify_0.c
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 17:54:38 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/07/13 03:29:11 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/07/13 10:26:12 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,33 @@ void	verify_a(char **chop)
 
 	i = 0;
 	verify_len(chop, 3);
-	printf(BCYN"A ok\n"RESET);
-	printf(BGRN"go check info\n\n"RESET);
+	printf(BCYN"A ok\n"RESET); //del
+	printf(BGRN"go check info\n\n"RESET); //del
 	if ((chop[1][0] == '0' || chop[1][0] == '1') && chop[1][1] == '.')
 	{
 		while (ft_atoi(&chop[1][2]) != i && i != 11)
 			i++;
 		if (i == 10 || (chop[1][0] == '1' && chop[1][2] != '0'))
-			error(BYEL"ratio not correct"RESET, NULL, 0);
+			error(BYEL"ratio not correct"RESET, 0);
 	}
 	else
-		error(BYEL"ratio not correct"RESET, NULL, 0);
-	printf(BMAG"ratio ok next\n"RESET);
-
+		error(BYEL"ratio not correct"RESET, 0);
+	printf(BMAG"ratio ok next\n"RESET); //del
 	color = ft_split(chop[2], ',');
 	i = 0;
 	verify_len(color, 3);
 	while (i < 3)
 	{
-		if (ft_atoi(color[i]) > 255 || color[i][0] == '-')
-			error(BYEL"color not correct"RESET, color, 0);
+		if (ft_atoi(color[i]) > 255 || color[i][0] == '-' || ft_strchr(color[i], '.') != NULL)
+			error(BYEL"color not correct"RESET, 0);
 		i++;
 	}
-	printf(BCYN"color ok, ambi checked. all ok\n"RESET);
+	printf(BCYN"color ok, ambi checked. all ok\n"RESET); //del
 }
 
 void	verify_cy(char **chop)
 {
 	verify_len(chop, 5);
-	printf(BCYN"CY ok\n"RESET);
-	printf(BGRN"go check info\n\n"RESET);
+	printf(BCYN"CY ok\n"RESET); //del
+	printf(BGRN"go check info\n\n"RESET); //del
 }
